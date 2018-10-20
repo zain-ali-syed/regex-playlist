b3823d8bc50142058b0f1c348a5444bf1a905e19
messagetobeats <chelnov63@hotmail.com>
2018-10-20 09:09:17 +0100

    first commit

--- validation.js
+++ validation.js
@@ -1,1 +1,24 @@
 // validation script here
+const aInputs = document.querySelectorAll('input');
+
+const regexPatterns =
+{
+    telephone: /^\d{11}$/,
+    username: /^\w{5,12}$/,
+    password: /^[@-\w]{8,20}$/,
+    slug: /^[a-z0-9-]{8,20}$/,
+    email:/^([\w-\.]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i
+}
+
+function validate(input_field, regex)
+{
+    if(regex.test(input_field.value)) input_field.className = 'valid'
+    else input_field.className = 'invalid';
+}
+
+aInputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        const input_field_name = e.target.attributes.name.value;
+        validate(e.target, regexPatterns[input_field_name] )
+    })
+})
